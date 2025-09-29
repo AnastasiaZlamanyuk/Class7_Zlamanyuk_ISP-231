71d224f4d29e5d3636923430dd6803c88b978f07
AnastasiaZlamanyuk <nastya.zlamanyuk@bk.ru>
2025-09-29 16:49:00 +0300

    change

---
+++ CardPicker.cs
@@ -0,0 +1,33 @@
+﻿internal class CardPicker
+{
+    static Random random = new Random();
+    public static string[] PickSomeCards(int numberOfCard)
+    {
+        string[] cards = new string[numberOfCard];
+        for (int i = 0; i < numberOfCard; i++)
+        {
+            cards[i] = RandomValue() + " " + RandomSuit();
+
+        }
+        return cards;
+    }
+
+    private static string RandomSuit()
+    {
+        int value = random.Next(1, 5);
+        if (value == 1) return "pik";
+        if (value == 2) return "cherve";
+        if (value == 3) return "tref";
+        return "byben";
+    }
+
+    private static string RandomValue()
+    {
+        int value = random.Next(1, 14);
+        if (value == 1) return "Tyz";
+        if (value == 11) return "Balet";
+        if (value == 12) return "Dama";
+        if (value == 13) return "Korol";
+        return value.ToString();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,34 +1,36 @@
-﻿namespace Class7_Zlamanyuk_ISP_231
+﻿using System;
+
+namespace Class7_Zlamanyuk_ISP_231
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //Student student = new Student();
-            //Student student1 = new Student("Mika");
-            //Student student2 = new Student("Mika",24);
-            //Student student3 = new Student("Mika",24, "ISP-231");
-            //Console.WriteLine(student);
-            //student.name = "Mika";
-            //student.age = 17;
-            //student.group = "ISP-231";
-            //student.PrintInfo();
-            //student1.name = "Veronika";
-            //student1.age = 18;
-            //student1.group = "ISP-231";
-            //student1.PrintInfo();
-            //Console.WriteLine($"{student.name} совершеннолетний - {student.IsAdult()}");
-            //Car car = new Car();
-            //car.model = "Skoda";
-            //car.Accelerate(80);
-            //Player player = new Player();
-            //Console.WriteLine(player.GetHealth());
-            //player.TakeDamage(40);
-            //TV tV = new TV();
-            //tV.SetVolume(78);
-            //tV.SetVolume(145);
-            //Person.Print();
-            //Console.WriteLine();
+            Student student = new Student();
+            Student student1 = new Student("Mika");
+            Student student2 = new Student("Mika", 24);
+            Student student3 = new Student("Mika", 24, "ISP-231");
+            Console.WriteLine(student);
+            student.name = "Mika";
+            student.age = 17;
+            student.group = "ISP-231";
+            student.PrintInfo();
+            student1.name = "Veronika";
+            student1.age = 18;
+            student1.group = "ISP-231";
+            student1.PrintInfo();
+            Console.WriteLine($"{student.name} совершеннолетний - {student.IsAdult()}");
+            Car car = new Car();
+            car.model = "Skoda";
+            car.Accelerate(80);
+            Player player = new Player();
+            Console.WriteLine(player.GetHealth());
+            player.TakeDamage(40);
+            TV tV = new TV();
+            tV.SetVolume(78);
+            tV.SetVolume(145);
+            Person.Print();
+            Console.WriteLine();
             BankAccount account = new BankAccount();
             account.Deposit(50);
             account.Withdraw(23);
@@ -38,13 +40,31 @@
             tom.CheckAge();
             Console.WriteLine(Personcs.retire);
             Personcs.retire = 67;
+            Console.Write("Введите количество карт для выбора: ");
+            string line = Console.ReadLine();
+            if (int.TryParse(line, out int num))
+            {
+                foreach (string card in CardPicker.PickSomeCards(num))
+                {
+                    Console.WriteLine(card);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Введите корректное число.");
+
+
+            }
+
+        }
+        class Person
+        {
+            static public void Print()
+            {
+                Console.WriteLine("Hello");
+            }
+
         }
     }
-    //class Person { 
-    //    static public void Print()
-    //    {
-    //        Console.WriteLine("Hello");
-    //    }
-    //}
 }
 
